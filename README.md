29b08ad8ed194276344f15728792a233edbb11b5
Wajahat Sarwat <165753648+wajahatsarwat@users.noreply.github.com>
2025-05-30 18:05:00 -0700

    socket room joined events

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 <!-- creating file wihout test -->
 # nest g resource user --no-spec
+
+
+<!--  fetched users in a room -->
+# const clients = await this.server.in('1').fetchSockets()
+# const count = clients.length;
--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,31 +1,45 @@
-import { ConnectedSocket, MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
-import { Server } from 'http';
-import { Socket } from 'socket.io';
+import { Logger } from '@nestjs/common';
+import { ConnectedSocket, MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets';
+import { Server, Socket } from 'socket.io';
 import { ChatService } from './chat.service';
 
-@WebSocketGateway({ namespace: 'chat', cors: true })
-export class ChatGateway {
+@WebSocketGateway({ cors: true })
+export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
+    private readonly logger = new Logger('MongoDB');
+
     @WebSocketServer() server: Server;
 
     constructor(private readonly chatService: ChatService) { }
 
     handleConnection(client: Socket) {
-        console.log(`Client connected: ${client.id}`);
+        const userId = client.handshake.query.userId;
+
+        client.join(String(userId))
+        this.logger.log(`Client connected: ${client.id}, userId = ${userId}`);
     }
 
+    handleDisconnect(client: Socket) {
+        this.logger.log(`Client disconnected: ${client.id}`);
+    }
+
+    // triggered for tests
     @SubscribeMessage('message')
     handleMessage(@MessageBody() data: any, @ConnectedSocket() client: Socket) {
         this.server.emit('message', data);
     }
 
+    // triggered when the user open the chat of any user
     @SubscribeMessage('joinRoom')
-    joinRoom(@MessageBody() data: { room: string }, @ConnectedSocket() client: Socket) {
-        client.join(data.room);
-        client.emit('joinedRoom', data.room);
+    async joinRoom(@MessageBody() data: { room: string }, @ConnectedSocket() client: Socket) {
+        client.join(String(data.room));
+
+        client.to(data.room).emit('joinedRoom', 'user joined your room');
     }
 
+    // triggered when user sends message 
     @SubscribeMessage('sendToRoom')
     handleSendToRoom(@MessageBody() data: { room: string, message: string }, @ConnectedSocket() client: Socket) {
         client.to(data.room).emit('roomMessage', data.message);
     }
+
 }
